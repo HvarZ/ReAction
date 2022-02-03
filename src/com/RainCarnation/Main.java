611af3c3be65f4090b48186a35793de8f4ec6c4f
HvarZ <hvarzahar@gmail.com>
2022-02-03 04:12:11 +0300

    init first javafx application

--- src/com/RainCarnation/Main.java
+++ src/com/RainCarnation/Main.java
@@ -1,8 +1,32 @@
 package com.RainCarnation;
 
-public class Main {
+import javafx.application.Application;
+import javafx.stage.Stage;
+import javafx.scene.Scene;
+import javafx.scene.Group;
+import javafx.scene.text.Text;
+
+public class Main extends Application {
 
     public static void main(String[] args) {
-        System.out.println("Hello");
+
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage stage) {
+
+        Text text = new Text("Hello!");
+        text.setLayoutY(80);
+        text.setLayoutX(125);
+
+        Group group = new Group(text);
+
+        Scene scene = new Scene(group);
+        stage.setScene(scene);
+        stage.setTitle("ReActionApp");
+        stage.setWidth(300);
+        stage.setHeight(250);
+        stage.show();
     }
 }
