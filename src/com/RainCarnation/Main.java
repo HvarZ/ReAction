33935c6b59a89303a3aadea9e59d04d4356f1791
HvarZ <hvarzahar@gmail.com>
2022-02-07 04:45:00 +0300

    first using fxml for front-end

--- src/com/RainCarnation/Main.java
+++ src/com/RainCarnation/Main.java
@@ -1,13 +1,14 @@
 package com.RainCarnation;
 
 import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
 import javafx.stage.Stage;
 import javafx.scene.Scene;
-import javafx.scene.Group;
-import javafx.scene.text.Text;
 import javafx.scene.paint.Color;
-import javafx.scene.control.Button;
+import javafx.scene.Parent;
 
+import java.io.IOException;
+import java.util.Objects;
 
 public class Main extends Application {
 
@@ -18,26 +19,21 @@ public class Main extends Application {
 
     @Override
     public void start(Stage stage) {
-        Button button = new Button();
-        button.setText("Click");
-        button.setMinSize(180, 90);
-        button.setLayoutX(160 - button.getMinWidth() / 2);
-        button.setLayoutY(280 - button.getMinHeight() / 2);
+        try  {
+            Parent root = FXMLLoader.load(Objects.requireNonNull(getClass().getResource("Main.fxml")));
 
+            Scene scene = new Scene(root);
 
-        button.setOnAction(e ->
-                button.setText("Clicked"));
+            scene.setFill(Color.BLACK);
 
-        Group group = new Group(button);
-        Scene scene = new Scene(group);
+            stage.setScene(scene);
+            stage.setWidth(320);
+            stage.setHeight(560);
+            stage.setTitle("ReActionApp");
 
-        scene.setFill(Color.BLACK);
-
-        stage.setScene(scene);
-        stage.setWidth(320);
-        stage.setHeight(560);
-        stage.setTitle("ReActionApp");
-
-        stage.show();
+            stage.show();
+        } catch (IOException e) {
+            System.err.println(e.getMessage());
+        }
     }
 }
