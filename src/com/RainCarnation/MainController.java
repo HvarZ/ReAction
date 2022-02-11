a30184d23d8f655a101155906d990a98063e7cc1
HvarZ <hvarzahar@gmail.com>
2022-02-11 21:32:29 +0300

    add changing color after clicking on main button

--- src/com/RainCarnation/MainController.java
+++ src/com/RainCarnation/MainController.java
@@ -2,11 +2,18 @@ package com.RainCarnation;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
 import javafx.scene.control.Label;
 import javafx.scene.control.Button;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.ResourceBundle;
 
 
-public class MainController {
+public class MainController implements Initializable {
     @FXML
     private Label averageResult;
 
@@ -17,18 +24,34 @@ public class MainController {
     private Button mainButton;
 
     private ButtonStatus status = ButtonStatus.Start;
+    private double singleResult;
+    private ArrayList<Double> times;
+
+    @FXML
+    private ImageView expectationImage;
 
     @FXML
     void click(ActionEvent event) {
         if (status == ButtonStatus.Start) {
-            mainButton.setText("Good job");
+            mainButton.setText("Wait a green color");
             status = ButtonStatus.Middle;
+            mainButton.setStyle("-fx-background-color: #fbff05");
         } else if (status == ButtonStatus.Middle) {
             mainButton.setText("Very good job");
-            status = ButtonStatus.Finish;
+            status = ButtonStatus.DoubleClick;
+            mainButton.setStyle("-fx-background-color: #19f749");
+        } else if (status == ButtonStatus.DoubleClick) {
+            mainButton.setText("Your result: " + singleResult);
+            status = ButtonStatus.Result;
         } else {
             mainButton.setText("Click to start...");
             status = ButtonStatus.Start;
+            mainButton.setStyle("-fx-background-color: silver");
         }
+    }
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        expectationImage = new ImageView(new Image("com/RainCarnation/assets/img_1.png"));
     }
 }
