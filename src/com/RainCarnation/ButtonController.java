ded39c5a1078995f02c54a342233d0ad1523cce8
HvarZ <hvarzahar@gmail.com>
2022-02-11 05:46:55 +0300

    create first visualisation of app

--- src/com/RainCarnation/ButtonController.java
+++ src/com/RainCarnation/ButtonController.java
@@ -1,18 +1,10 @@
 package com.RainCarnation;
 
-import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 
 
 public class ButtonController {
     @FXML
-    private Button button;
-    private int counter = 0;
-    
-    @FXML
-    private void click(ActionEvent action) {
-        button.setText(Integer.toString(counter));
-        counter++;
-    }
+    private Button mainButton;
 }
--- src/com/RainCarnation/Main.java
+++ src/com/RainCarnation/Main.java
@@ -1,11 +1,9 @@
 package com.RainCarnation;
 
 import javafx.application.Application;
-import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.stage.Stage;
 import javafx.scene.Scene;
-import javafx.scene.paint.Color;
 import javafx.scene.Parent;
 
 import java.util.Objects;
@@ -21,15 +19,12 @@ public class Main extends Application {
     @Override
     public void start(Stage stage) throws Exception {
         Parent root = FXMLLoader.load(Objects.requireNonNull(getClass().getResource("Main.fxml")));
-        root.setLayoutX(230);
-        root.setLayoutY(270);
         Scene scene = new Scene(root);
-        scene.setFill(Color.BLACK);
 
         stage.setScene(scene);
         stage.setTitle("ReActionApp");
-        stage.setWidth(460);
-        stage.setHeight(540);
+        stage.setWidth(450);
+        stage.setHeight(600);
 
         stage.show();
     }
