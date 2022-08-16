804593e181b1b5d9a46fa429339df065430ccb17
hvarz <hvarzahar@gmail.com>
2022-08-16 17:39:44 +0300

    add timeline and refactor if-else to switch-case

--- src/com/RainCarnation/MainController.java
+++ src/com/RainCarnation/MainController.java
@@ -1,9 +1,13 @@
 package com.RainCarnation;
 
+import javafx.animation.KeyFrame;
+import javafx.animation.Timeline;
 import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
 import javafx.scene.control.Button;
+import javafx.util.Duration;
 
 import java.util.ArrayList;
 import java.util.Collections;
@@ -20,46 +24,67 @@ public class MainController {
     private Button mainButton;
 
     private ButtonStatus status = ButtonStatus.Start;
-    private ArrayList<Long> times = new ArrayList<>();
+    private ButtonStatus time = ButtonStatus.Yellow;
+    private final ArrayList<Long> times = new ArrayList<>();
     private final Random randomEngine = new Random();
     private long startTime;
+    private final Timeline banDuration = new Timeline(new KeyFrame(
+            Duration.millis(randomEngine.nextInt(2000) + 1000),
+            new EventHandler<>() {
+                @Override
+                public void handle(ActionEvent actionEvent) {
+                    mainButton.setText("Click it fast!!!");
+                    mainButton.setStyle("-fx-background-color: #19f749");
+                    startTime = System.currentTimeMillis();
+                    time = ButtonStatus.Green;
+                }
+            }));
 
     @FXML
     public void click(ActionEvent event) throws Exception {
-        if (status == ButtonStatus.Start) {
-            mainButton.setText("Wait a green color");
-            status = ButtonStatus.Middle;
-            mainButton.setStyle("-fx-background-color: #fbff05");
-        } else if (status == ButtonStatus.Middle) {
-            Thread.sleep(randomEngine.nextInt(2000) + 1000);
-            mainButton.setText("Click it fast!!!");
-            status = ButtonStatus.DoubleClick;
-            mainButton.setStyle("-fx-background-color: #19f749");
-            startTime = System.currentTimeMillis();
-        } else if (status == ButtonStatus.DoubleClick) {
-            long singleResult = System.currentTimeMillis() - startTime;
-            mainButton.setText("Your result: " + singleResult + "ms" + " (" + (times.size() + 1) + "/5)");
-            status = ButtonStatus.Result;
-            times.add(singleResult);
-            long sum = 0;
-            for (long time : times) {
-                sum += time;
+        switch (status) {
+            case Start -> {
+                mainButton.setText("Wait a green color");
+                mainButton.setStyle("-fx-background-color: #fbff05");
+                status = ButtonStatus.Yellow;
+                time = ButtonStatus.Yellow;
+                banDuration.setCycleCount(Timeline.INDEFINITE);
+                banDuration.playFromStart();
             }
-            averageResult.setText("Average: " + sum / times.size() + "ms");
-            bestResult.setText("Best: " + Collections.min(times) + "ms");
-            if (times.size() == 5) {
-                times.clear();
+
+            case Yellow -> {
+                if (time == ButtonStatus.Yellow) {
+                    banDuration.stop();
+                    status = ButtonStatus.Start;
+                    mainButton.setText("Too early... Try again!");
+                    mainButton.setStyle("-fx-background-color: #ff4a12");
+                } else {
+                    long singleResult = System.currentTimeMillis() - startTime;
+                    banDuration.stop();
+                    mainButton.setText("Your result: " + singleResult + "ms" + " (" + (times.size() + 1) + "/5)");
+                    status = ButtonStatus.Result;
+                    times.add(singleResult);
+                    long sum = 0;
+                    for (long time : times) {
+                        sum += time;
+                    }
+                    averageResult.setText("Average: " + sum / times.size() + "ms");
+                    bestResult.setText("Best: " + Collections.min(times) + "ms");
+                    if (times.size() == 5) {
+                        times.clear();
+                    }
+                }
             }
-        } else {
-            if (times.size() == 0) {
-                averageResult.setText("Average: ~");
-                bestResult.setText("Best: ~");
+
+            case Result -> {
+                if (times.size() == 0) {
+                    averageResult.setText("Average: ~");
+                    bestResult.setText("Best: ~");
+                }
+                mainButton.setText("Click to start...");
+                status = ButtonStatus.Start;
+                mainButton.setStyle("-fx-background-color: silver");
             }
-            mainButton.setText("Click to start...");
-            status = ButtonStatus.Start;
-            mainButton.setStyle("-fx-background-color: silver");
         }
     }
 }
-
-// problems with multiclick and second stage
